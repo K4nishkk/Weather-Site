44bafa06285e23771d7743fecae3fdc8c70461fd
K4nishkk <115213375+K4nishkk@users.noreply.github.com>
2023-06-18 13:14:39 +0530

    hour and days forecast added

--- w.js
+++ w.js
@@ -12,7 +12,6 @@ async function getWeatherData(API_type, parameters) {
 async function getIPDetails() {
     const response = await fetch('http://ip-api.com/json/');
     const jsonData = await response.json();
-    console.log(jsonData);
     return jsonData;
 }
 
@@ -48,7 +47,7 @@ function showCity() {
             for (let i = 0; i < weatherData.length; i++) {
 
                 const option = document.createElement('option');
-                option.setAttribute('value', weatherData[i].name);
+                option.setAttribute('value', weatherData[i].name + ', ' + weatherData[i].region);
 
                 datalist.appendChild(option);
             }
@@ -67,4 +66,58 @@ function displayCityData() {
     )
 }
 
-document.getElementById('temp_c').innerHTML = 'meow';
+// get date in YYYY-MM-DD format
+function futureDates(n) {
+    var date = new Date(new Date().getTime() + n * 24 * 60 * 60 * 1000);
+
+    // getMonth gives value b/w 0 - 11, so date.getMonth() + 1
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
+
+// future forecast function
+function dayForecast() {
+    const futureDays = document.getElementById('futureDays');
+    for (let i = 1; i < 8; i++) {
+        const parameters = 'q=chandigarh&date=' + futureDates(i);
+        getWeatherData('forecast', parameters).then (
+            (weatherData) => {
+                const futureDaysChild = document.createElement('div');
+                const node = document.createTextNode(weatherData.forecast.forecastday[0].date + '  ' + weatherData.forecast.forecastday[0].day.maxtemp_c);
+                futureDaysChild.appendChild(node);
+                futureDays.appendChild(futureDaysChild);
+            }
+        )
+    }
+}
+
+dayForecast();
+
+function hourForecast() {
+    const futureHours = document.getElementById('futureHours');
+
+    // getting today's and tommorow's data for 24 hour forecast
+    const parameters = 'q=chandigarh&days=2';
+
+    getWeatherData('forecast', parameters).then(
+        (weatherData) => {
+            const h = new Date().getHours() + 1; // next hour
+            for (let i = 0; i < 24; i++) {
+
+                let day = 0; // today
+                let hour = i + h;
+
+                if ((i + h) / 24 >= 1) {
+                    day = 1; // tommorow
+                    hour = (i + h) % 24;
+                }
+
+                const futureHoursChild = document.createElement('div');
+                const node = document.createTextNode(weatherData.forecast.forecastday[day].hour[hour].temp_c);
+                futureHoursChild.appendChild(node);
+                futureHours.appendChild(futureHoursChild);
+            }
+        }
+    )
+}
+
+hourForecast();
